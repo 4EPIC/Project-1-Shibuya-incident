ee37c2e159adc5afa624a9bf6cf232ff2334b31d
Eric Esteban <eric_7@hotmail.com>
2021-11-04 10:40:05 +0100

    final

--- js/player.js
+++ js/player.js
@@ -1,17 +1,12 @@
 "use strict";
 
 class Player {
-  constructor(canvas, lives) {
+  constructor(canvas) {
     this.canvas = canvas;
     this.ctx = this.canvas.getContext("2d");
-    // this.canvasHeight = 600;
-    this.lives = lives;
     this.size = 60;
     this.x = canvas.width / 2 - this.size / 2;
     this.y = canvas.height / 2 - this.size / 2;
-    this.vx = 0;
-    this.vy = 0;
-    this.r = 0;
     this.move = 5;
     this.canUp = true;
     this.canDown = true;
@@ -20,50 +15,11 @@ class Player {
     this.direction = null;
     this.timeBullet = true;
     this.bulletsArray = new Array();
-    this.friction = 0.89;
     this.imagePj = new Image();
     this.imagePj.src = "../images/spriteGojo3.png";
-    // this.speed = 1;
-  }
-
-  damage() {
-    this.health -= 1;
-    if (this.health <= 0) {
-      this.alive = false;
-      this.Obstacle.kill();
-      return true;
-    }
-    return false;
   }
 
   update() {
-    //update speed
-    //this.vx += this.ax;
-    //this.vy += this.ay;
-
-    //cheat's friction (friction = 0.97)
-    //this.vx *= this.friction;
-    //this.vy *= this.friction;
-
-    // if(this.vx > 1.8) this.vx = 1.8
-    // // if(this.vx < -0.5) this.vx = -0.5
-    // if(this.vy > 1.8) this.vy = 1.8
-    // // if(this.vy < -0.5) this.vy = -0.5
-
-    //update position
-    /* if (this.vx) {
-       this.x += 5;
-     }
-     if (!this.vx) {
-       this.x -= 5;
-     }
-     if (this.vy) {
-       this.y += 5;
-     }
-     if (this.vy) {
-       this.y -= 5;
-     }*/
-
     if (this.canMoveUp && this.direction == "up") {
       this.y -= this.move;
     }
